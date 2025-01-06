7dc34db8c00361e439c44b2f89dc165cdbc97453
danday74 <daniellewis777@gmail.com>
2025-01-06 12:30:59 +0000

    services

---
+++ src/app/routes/basic/services/basic/basic.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing'
+import { BasicService } from './basic.service'
+
+describe('BasicService', () => {
+  let service: BasicService
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(BasicService)
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy()
+  })
+})
