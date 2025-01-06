bff055bcab834892b163a8d84bdc6916c9aae701
danday74 <daniellewis777@gmail.com>
2025-01-06 12:46:59 +0000

    basic service

--- src/app/routes/basic/components/basic/basic.component.ts
+++ src/app/routes/basic/components/basic/basic.component.ts
@@ -1,10 +1,9 @@
-import { Component, computed, inject, input, resource } from '@angular/core'
+import { Component, computed, inject, input, InputSignal, resource, ResourceRef, Signal } from '@angular/core'
 import { ButtonsComponent } from '@components/buttons/buttons.component'
-import { users } from '@routes/basic/data/users'
-import { find } from 'lodash-es'
-import { IUser } from '@interfaces/i-user'
 import { UserComponent } from '@components/user/user.component'
 import { Router } from '@angular/router'
+import { BasicService } from '@routes/basic/services/basic/basic.service'
+import { IUser } from '@interfaces/i-user'
 
 @Component({
   selector: 'app-basic',
@@ -16,19 +15,17 @@ import { Router } from '@angular/router'
   styleUrl: './basic.component.scss',
 })
 export class BasicComponent {
-  paramUid = input<string>(null) // route param
+  paramUid: InputSignal<string> = input<string>(null) // route param
 
-  private uid = computed<number>(() => parseInt(this.paramUid(), 10))
+  private uid: Signal<number> = computed<number>((): number => parseInt(this.paramUid(), 10))
 
-  user = resource({
+  private router: Router = inject(Router)
+  private basicService: BasicService = inject(BasicService)
+
+  user: ResourceRef<IUser> = resource({
     request: this.uid,
-    loader: ({ request: uid }) => {
-      const user: IUser = find(users, { id: uid })
-      return Promise.resolve(user)
-    },
+    loader: this.basicService.basicLoader,
   })
-
-  private router = inject(Router)
 
   buttonClick(idx: number) {
     this.router.navigateByUrl(`/basic/${idx}`).then()
--- src/app/routes/basic/services/basic/basic.service.ts
+++ src/app/routes/basic/services/basic/basic.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@angular/core'
+import { Injectable, ResourceLoaderParams } from '@angular/core'
+import { IUser } from '@interfaces/i-user'
+import { find } from 'lodash-es'
+import { users } from '@routes/basic/data/users'
 
 @Injectable({ providedIn: 'root' })
-export class BasicService {}
+export class BasicService {
+  basicLoader(params: ResourceLoaderParams<number>): Promise<IUser> {
+    const { request: uid } = params
+    return new Promise((resolve) => {
+      const user: IUser = find(users, { id: uid })
+      resolve(user)
+    })
+  }
+}
